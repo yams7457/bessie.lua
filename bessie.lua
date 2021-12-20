cdfe85cfa198b2b1a3d52c7ce21c34ae1b54d8ee
yams7457 <68620179+yams7457@users.noreply.github.com>
2021-12-20 07:39:55 -0600

    Add files via upload

--- bessie.lua
+++ bessie.lua
@@ -3,12 +3,12 @@ m = midi.connect()
 m.event = function(data)   
   local d = midi.to_msg(data)
   if d.type == "note_on" then
-      if #played_notes >= 4 then
+      if tab.count(played_notes) >= 4 then
         for k in pairs (played_notes) do
           played_notes[k] = nil
         end
       end
-      played_notes[#played_notes + 1] = d.note % 12
+      played_notes[tab.count(played_notes) + 1] = d.note % 12
     for k,v in spairs(played_notes, function(t,a,b) return t[b] > t[a] end) do
       bottom_to_top[k] = v
     end
@@ -65,6 +65,20 @@ chord_table[1][4][7][9] = "A7"
 chord_table[2][5][8][10] = "Bb7"
 chord_table[3][6][9][11] = "B7"
 
+--here's some major 7 chords
+chord_table[0][4][7][11] = "CM7"
+chord_table[0][1][5][8] = "DbM7"
+chord_table[1][2][6][9] = "DM7"
+chord_table[2][3][7][10] = "EbM7"
+chord_table[3][4][8][11] = "EM7"
+chord_table[0][4][5][9] = "FM7"
+chord_table[1][5][6][10] = "GbM7"
+chord_table[2][6][7][11] = "GM7"
+chord_table[0][3][7][8] = "AbM7"
+chord_table[1][4][8][9] = "AM7"
+chord_table[2][5][9][10] = "BbM7"
+chord_table[3][6][10][11] = "BM7"
+
 --here's some major 6 chords
 chord_table[0][4][7][9] = "CM6"
 chord_table[1][5][8][10] = "DbM6"
@@ -116,8 +130,8 @@ function key (n,z)
 end
 
 function tell_me_what_i_played()
-  if #played_notes == 4 then
-    print(chord_table[played_notes[1]][played_notes[2]][played_notes[3]][played_notes[4]])
+  if tab.count(played_notes) == 4 then
+    print(chord_table[bottom_to_top[1]][bottom_to_top[2]][bottom_to_top[3]][bottom_to_top[4]])
     calculate_a_new_chord()
     redraw()
   end
@@ -137,9 +151,9 @@ function calculate_a_new_chord()
     end
   end
   for i = 1,4 do
-    note_options[3][i] = (played_notes[i] + 1) % 12
-    note_options[2][i] = (played_notes[i] - 1) % 12
-    note_options[1][i] = played_notes[i]
+    note_options[3][i] = (bottom_to_top[i] + 1) % 12
+    note_options[2][i] = (bottom_to_top[i] - 1) % 12
+    note_options[1][i] = bottom_to_top[i]
   end
   for i = 1,3 do
     for j = 1,3 do
@@ -156,7 +170,7 @@ function calculate_a_new_chord()
       end
     end
   end
-  new_chord = limited_chord_choices[math.random(1, #limited_chord_choices)]
+  new_chord = limited_chord_choices[math.random(1, tab.count(limited_chord_choices))]
 end
 
 
@@ -185,10 +199,10 @@ end
 
 function redraw()
   screen.clear()
-  screen.move(0,40)
+  screen.move(0,20)
   screen.level(15)
-  screen.text(chord_table[played_notes[1]][played_notes[2]][played_notes[3]][played_notes[4]])
-  screen.move(60,40)
+  screen.text(chord_table[bottom_to_top[1]][bottom_to_top[2]][bottom_to_top[3]][bottom_to_top[4]])
+  screen.move(0,40)
   screen.text(new_chord)
   for i = 1,4 do
     screen.move(i * 10, 10)
